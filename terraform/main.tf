0371e65dece66f24aa35eaca6919626ea69985c6
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 <Miss.Jaya@kpmg.co.uk>
2022-11-18 13:38:24 +0530

    corrected subnet address prefixes

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,7 +30,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "paperchase-subnet"
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.virtual_network_name
-  address_prefix       = "10.0.0.0/24"
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_key_vault" "kv" {
