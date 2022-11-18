1d392203daafcfb3966d608032b1c2e22c06f559
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 <Miss.Jaya@kpmg.co.uk>
2022-11-18 14:03:56 +0530

    correcting depends_on in subnet block

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,8 +35,8 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = var.virtual_network_name
   address_prefixes     = ["10.0.1.0/24"]
   depends_on = [
-    azurerm_resource_group.rg.name,
-    azurerm_virtual_network.vnet.name,
+    azurerm_resource_group.rg,
+    azurerm_virtual_network.vnet,
   ]
 }
 
