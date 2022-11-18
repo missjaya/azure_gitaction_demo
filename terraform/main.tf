5348d534392cb4d14162d64c6bf3c2e813cf0d92
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 <Miss.Jaya@kpmg.co.uk>
2022-11-18 13:23:32 +0530

    formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,26 +6,26 @@ terraform {
     key                  = "tfstatedemo.tfstate"
   }
 }
- 
+
 provider "azurerm" {
   version = "~>2.0"
   features {}
 }
 
 data "azurerm_client_config" "current" {}
- 
+
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.region
 }
- 
+
 resource "azurerm_virtual_network" "vnet" {
   name                = "paperchase-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = "eastus2"
   resource_group_name = var.resource_group_name
 }
- 
+
 resource "azurerm_subnet" "subnet" {
   name                 = "paperchase-subnet"
   resource_group_name  = var.resource_group_name
